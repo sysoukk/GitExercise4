1613182e0156b30d25ee31355e9d860934a0459f
SyrusSoukkaserm <syrus.soukkaserm@my.century.edu>
2025-05-03 22:46:49 -0500

    deleted all the programs from initial commit and added a git exercise 4 program

---
+++ GitExercise4/GitExercise4/GitExercise4.cpp
@@ -0,0 +1,24 @@
+// GitExercise4.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "This is exercise 4 of using Git\n";
+
+    cout << "I will be using a online repository, GitHub, to push all my commits to. \n";
+    cout << "First I will delete all the programs from the initial commit and add this one. \n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
